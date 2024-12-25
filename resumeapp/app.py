3551a3a6f4a7ec8d598bf1524f044fe69d25afd0
Arti <Arti@example.com>
2024-12-25 14:19:53 +0530

    updated app.py

--- resumeapp/app.py
+++ resumeapp/app.py
@@ -45,5 +45,5 @@ def home():
     return render_template_string(html_template, resume=resume)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=80)
 
