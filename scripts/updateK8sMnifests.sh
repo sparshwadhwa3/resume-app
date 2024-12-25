213d8dabd071751450be6131134c04ffe0a16417
Arti <Arti@example.com>
2024-12-25 13:52:18 +0530

    added update script

---
+++ scripts/updateK8sMnifests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+REPO_URL="https://Bvyn3laGEmdLhyy3hK2UCBMf7b1GjIuGhbB0PKTUrSUKV7yBXASRJQQJ99ALACAAAAAAAAAAAAASAZDOyjNh@dev.azure.com/sparshwadhwadevops/voteapp/_git/voteapp"
+git clone "$REPO_URL" /tmp/temp_repo
+cd /tmp/temp_repo
+sed -i "s|image:.*|image: resumecr.azurecr.io/$2:$3|g" k8s-specifications/$1-deployment.yaml
+git add .
+git commit -m "update K8s Manifests"
+git push
+rm -rf /tmp/temp_repo
